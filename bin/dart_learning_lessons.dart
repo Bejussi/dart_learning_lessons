c0f08f36b0f0499fcfed96045855f43d7c2d60e9
Alisa <alisadmsiis@gmail.com>
2026-03-13 18:41:58 +0200

    initial commit

---
+++ bin/dart_learning_lessons.dart
@@ -0,0 +1,5 @@
+import 'package:dart_learning_lessons/dart_learning_lessons.dart' as dart_learning_lessons;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_learning_lessons.calculate()}!');
+}
---
+++ test/dart_learning_lessons_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_learning_lessons/dart_learning_lessons.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
